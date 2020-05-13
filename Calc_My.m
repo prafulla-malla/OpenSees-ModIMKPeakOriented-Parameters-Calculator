8dfbd630992413a21d913b79194f7cda5e525f57
vincenzot <enzo.t.1@hotmail.it>
2020-05-13 15:09:44 +0200

    Adding MIT license in calc_my

--- Calc_My.m
+++ Calc_My.m
@@ -1,3 +1,12 @@
+% MIT License
+
+% Copyright (c) 2020 Vincenzo Tartaglia
+
+% Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+% The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 function calc_my = Calc_My(N,myRectangularSection, myConcrete, mySteel)
     Ec = myConcrete.Ec;
     Es = mySteel.E0;
@@ -35,7 +44,7 @@ function calc_my = Calc_My(N,myRectangularSection, myConcrete, mySteel)
 
         
     % Reinforcement ratios
-    % In questo caso è uguale perchè il numero dei ferri è lo stesso
+    % In questo caso Ã¨ uguale perchÃ¨ il numero dei ferri Ã¨ lo stesso
     rho_s = myRectangularSection.getRhoTop(); % tension
     rho_1s = myRectangularSection.getRhoBottom(); % compression
     
